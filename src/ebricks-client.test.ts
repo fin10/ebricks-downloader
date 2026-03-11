24a75177a76c27db71aa6df2aed5fa1b714a4b58
fin10 <fender64@gmail.com>
2026-03-11 23:53:27 +0900

    Add test case for download

--- src/ebricks-client.test.ts
+++ src/ebricks-client.test.ts
@@ -2,10 +2,24 @@ import { expect, test } from "vitest";
 import { EbricksClient } from "./ebricks-client.js";
 import { config } from "./config.js";
 
-test.skipIf(!config.getBoolean("MANUAL_TEST"))(``, async () => {
+const isManualTestEnabled = config.getBoolean("MANUAL_TEST_ENABLED");
+
+test.skipIf(!isManualTestEnabled)(`Get topics`, async () => {
   const client = new EbricksClient();
 
   const topics = await client.getTopics({ seq: 128, unit: 0 });
 
   expect(topics.length).toBeGreaterThan(0);
+});
+
+test.skipIf(!isManualTestEnabled)(`Download topic`, async () => {
+  const client = new EbricksClient();
+
+  const [topic] = (await client.getTopics({ seq: 128, unit: 0 })) || [];
+
+  expect(topic).toBeDefined();
+
+  const stream = await client.download(topic);
+
+  expect(stream).toBeDefined();
 });
